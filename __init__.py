758bfb32df51bdc7df8a702fab364b28e75fdb0c
Zhongxia Yan <zxyan@berkeley.edu>
2018-12-07 13:39:30 -0500

    added reindex and imports

--- __init__.py
+++ __init__.py
@@ -1,28 +1,34 @@
+using_ipython = True
+try:
+    shell = get_ipython().__class__.__name__
+except NameError:
+    using_ipython = False
+
+import subprocess, os, re, tempfile, zipfile, gzip, io, shutil, string, random, itertools, pickle, json
+from time import time
+from glob import glob
+from datetime import datetime
+import warnings
+warnings.filterwarnings('ignore')
+
 import numpy as np
-import subprocess, os, re, time, zipfile, gzip, io, shutil, string, random, itertools, pickle, json, codecs
+import pandas as pd
+if not using_ipython:
+    import matplotlib
+    matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import scipy.stats
+import scipy as sp
+from scipy.stats import pearsonr as pearson, spearmanr as spearman, kendalltau
+from sklearn.metrics import roc_auc_score as auroc, average_precision_score as auprc, roc_curve as roc, precision_recall_curve as prc, accuracy_score as accuracy
 
 def get_name(path, ext=True):
     name = os.path.basename(path)
     if ext: return name
     else: return os.path.splitext(name)[0]
 
-def list_dir(dir_, ext, return_name=False):
-    if ext == '/':
-        criteria = lambda x: os.path.isdir(os.path.join(dir_, x))
-        strip_ext = lambda x: x
-        post_process = lambda x: x + '/'
-    else:
-        if ext[0] != '.': ext = '.' + ext.lower()
-        criteria = lambda x: x[-len(ext):].lower() == ext
-        strip_ext = lambda x: get_name(x, ext=False)
-        post_process = lambda x: x
-    files = (f for f in os.listdir(dir_) if criteria(f))
-    files = sorted((strip_ext(file), post_process(os.path.join(dir_, file))) for file in files)
-    if return_name: return files
-    else: return [path for file, path in files]
-
 def load_json(path):
-    with open(path, 'rb') as f:
+    with open(path, 'r+') as f:
         return json.load(f)
 
 def save_json(dict_, path):
@@ -44,11 +50,6 @@ def load_pickle(path):
 def save_pickle(obj, path):
     with open(path, 'wb') as f:
         pickle.dump(obj, f)
-
-def make_dir(path):
-    if not os.path.exists(path):
-        os.makedirs(path)
-    return path
 
 def remove(path):
     if not os.path.exists(path):
@@ -75,13 +76,6 @@ def extract(input_path, output_path=None):
     else:
         raise RuntimeError('Don\'t know file extension for ' + input_path)
 
-def get_temp_file(ext, N=10, parent_dir=None):
-    if parent_dir is None: parent_dir = '/tmp/zxyan_temp'
-    return parent_dir + ''.join(random.sample(string.digits, k=N)) + ext
-
-def get_temp_dir(N=10):
-    return get_temp_file('/', N)
-
 def shell(cmd, wait=True, ignore_error=2):
     if type(cmd) != str:
         cmd = ' '.join(cmd)
@@ -89,15 +83,7 @@ def shell(cmd, wait=True, ignore_error=2):
     if not wait:
         return process
     out, err = process.communicate()
-    if err:
-        if ignore_error == 2:
-            pass
-        elif ignore_error:
-            print(err.decode())
-        else:
-            print(err.decode())
-            raise RuntimeError('Error in command line call')
-    return out.decode()
+    return out.decode(), err.decode() if err else None
 
 def parallel_execution(process_fn, n_jobs, error_msg=''):
     '''
@@ -118,3 +104,55 @@ def attributes(obj):
     import inspect, pprint
     pprint.pprint(inspect.getmembers(obj, lambda a: not inspect.isroutine(a)))
 
+def reindex(df, order=None, rename=None, level=[], axis=0, squeeze=True):
+    assert axis in [0, 1]
+    if type(level) is not list:
+        if order is not None: order = [order]
+        if rename is not None: rename = [rename]
+        level = [level]
+    if order is None: order = [[]] * len(level)
+    if rename is None: rename = [{}] * len(level)
+    assert len(level) == len(rename) == len(order)
+    multiindex = df.index
+    if axis == 1:
+        multiindex = df.columns
+    for i, (o, lev) in enumerate(zip(order, level)):
+        if len(o) == 0:
+            seen = set()
+            new_o = []
+            for k in multiindex.get_level_values(lev):
+                if k in seen: continue
+                new_o.append(k)
+                seen.add(k)
+            order[i] = new_o
+    assert len(set(level) - set(multiindex.names)) == 0, 'Levels %s not in index %s along axis %s' % (level, axis, multiindex.names)
+    lev_order = dict(zip(level, order))
+    level_map = {}
+    for lev in multiindex.names:
+        if lev in level:
+            level_map[lev] = { name : i for i, name in enumerate(lev_order[lev]) }
+        else:
+            index_map = {}
+            for x in multiindex.get_level_values(lev):
+                if x in index_map: continue
+                index_map[x] = len(index_map)
+            level_map[lev] = index_map
+    tuples = list(multiindex)
+    def get_numerical(tup):
+        return tuple(level_map[lev][t] for t, lev in zip(tup, multiindex.names))
+    filtered_tuples = [tup for tup in tuples if all(t in level_map[lev] for t, lev in zip(tup, multiindex.names))]
+    new_tuples = sorted(filtered_tuples, key=get_numerical)
+    lev_rename = dict(zip(level, rename))
+    renamed_tuples = [tuple(lev_rename.get(lev, {}).get(t, t) for t, lev in zip(tup, multiindex.names)) for tup in new_tuples]
+    new_index = pd.MultiIndex.from_tuples(new_tuples, names=multiindex.names)
+    renamed_index = pd.MultiIndex.from_tuples(renamed_tuples, names=multiindex.names)
+    if squeeze:
+        single_levels = [i for i, level in enumerate(renamed_index.levels) if len(level) == 1]
+        renamed_index = renamed_index.droplevel(single_levels)
+    if axis == 0:
+        new_df = df.loc[new_index]
+        new_df.index = renamed_index
+    else:
+        new_df = df.loc[:, new_index]
+        new_df.columns = renamed_index
+    return new_df
